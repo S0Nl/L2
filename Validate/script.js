68ee1cfd904eb5360946f3819d60e9120e2b40c5
S0NI <yorikyorik25@rambler.ru>
2016-06-15 10:41:45 +0300

    first commit

---
+++ Validate/script.js
@@ -0,0 +1,12 @@
+function isnum() {
+var nm = document.getElementById("num").value;
+    if ( nm  == 0 ) return true;
+    return res = ( nm /nm ) ? true : false;
+}
+function lb3(){
+if(isnum()){ 
+	alert(document.getElementById("full").value);}
+else
+alert('Its not a number!');
+
+}
---
+++ script.js
@@ -0,0 +1,40 @@
+﻿function Concert(exhibition, location, organizer, date) {
+    this.exhibition = exhibition;
+    this.location = location;
+    this.organizer = organizer;
+    
+    this.validateExhibition = function() {
+        if (this.exhibition.length == 0) {
+            alert("\Невідома назава предмету");
+        }
+    }
+    this.validateLocation = function() {
+        if (this.location.length == 0) {
+            alert(" \ Невідоме місце проведення консультації");
+        }
+    }
+		
+    this.validateOrganizer = function() {
+	var now = new Date();	
+	if (this.organizer == now.getDate()) {
+            alert(" \ Консультація проходить сьогодні");
+        }else{ 
+		if (this.organizer.length == 0){
+		alert("Невідома дата консультації / Екзамену") }}
+    }
+}
+function validate() {
+    var inputData = new Array(
+        document.getElementById("exhibition").value,
+        document.getElementById("location").value,
+        document.getElementById("organizer").value
+    );
+
+    var concert = new Concert(inputData[0], inputData[1], inputData[2]);
+
+    concert.validateExhibition();
+    concert.validateLocation();
+    concert.validateOrganizer();
+}
+
+
